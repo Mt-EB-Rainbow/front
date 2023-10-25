d2a3eb5101dc7e790710b146aa8d07ec3d18730e
hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2023-10-25 19:24:14 +0900

    fix: layout 단위 수정

--- src/components/_common/layout/Footer.style.js
+++ src/components/_common/layout/Footer.style.js
@@ -2,22 +2,22 @@ import styled from 'styled-components';
 
 export const FooterBox = styled.div`
     width: 100vw;
-    height: 100px;
+    height: 9.2vh;
     background-color: #f5f5f5;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
     position: relative;
-    /* transform: translateY(-100%); */
+    bottom: 0;
 `;
 
 export const Text = styled.span`
-    font-size: 12px;
+    font-size: 1.1vh;
     font-family: var(--korean);
     color: #767676;
-    margin-left: 48px;
-    margin-right: 48px;
+    margin-left: 3.3vw;
+    margin-right: 3.3vw;
     & > a {
         color: #767676;
     }
