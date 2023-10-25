723af37cdff147a41c8e98d41e1f99cb2d93d656
hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2023-10-25 19:25:35 +0900

    add: mentorPage

---
+++ src/pages/MentorRegistPage.jsx
@@ -0,0 +1,11 @@
+import MentorRegist from '../components/mentorpage/MentorRegist';
+
+const MentorRegistPage = () => {
+    return (
+        <>
+            <MentorRegist />
+        </>
+    );
+};
+
+export default MentorRegistPage;
