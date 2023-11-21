b76a2677d67f782c3297c0c74fbb49be05fcfc99
aoqlsdl <hyeeev@gmail.com>
2023-11-21 21:21:05 +0900

    feat: training 페이지 생성

---
+++ src/pages/TrainingPage.jsx
@@ -0,0 +1,11 @@
+import Training from '../components/trainingpage/Training';
+
+const TrainingPage = () => {
+    return (
+        <>
+            <Training />
+        </>
+    );
+};
+
+export default TrainingPage;
