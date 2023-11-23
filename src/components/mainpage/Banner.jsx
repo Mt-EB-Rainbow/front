5ecf7ef2a96b810bcd38774c17c622eaff89d3c9
hanby-choi <hanby.choi@ewhain.net>
2023-11-23 23:16:51 +0900

    add: 임시 배너 이미지 추가

--- src/components/mainpage/Banner.jsx
+++ src/components/mainpage/Banner.jsx
@@ -1,7 +1,8 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import banner from '../../assets/mainpage/banner.png';
+import banner from '../../assets/mainpage/banner_txt.png';
+import styled from 'styled-components';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -38,3 +39,41 @@ export default function Banner() {
         </>
     );
 }
+
+const Container = styled.div`
+    @media (max-width: 30rem) {
+        height: 15rem;
+    }
+    width: 100%;
+    height: 30rem;
+    position: relative;
+    z-index: 0;
+`;
+
+const BannerTxt = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 0;
+    right: 0;
+    width: 50rem;
+    opacity: 0;
+    margin: auto;
+    text-align: center;
+    transition: 0.5s;
+    z-index: 10;
+
+    .title {
+        position: absolute;
+        color: var(--black);
+        font-size: 1.4rem;
+        line-height: 1.25;
+        font-weight: bold;
+        margin-bottom: 0.8rem;
+        z-index: 100;
+    }
+
+    .text {
+        color: var(--white);
+        font-size: 4rem;
+    }
+`;
