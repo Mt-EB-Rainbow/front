32e5a90cd629ea2623f37451d6ca98d024bf5d1c
hanby-choi <hanby.choi@ewhain.net>
2023-11-23 22:29:12 +0900

    feat: 메인 페이지 단위 수정 및 퍼블리싱 (일반/멘토)

--- src/components/mainpage/Main.jsx
+++ src/components/mainpage/Main.jsx
@@ -1,29 +1,82 @@
 import { S } from './Main.style';
 import Banner from './Banner';
+import MenuBtn from './MenuBtn';
+import dic from '../../assets/mainpage/dic.svg';
+import child from '../../assets/mainpage/child.svg';
+import edu from '../../assets/mainpage/edu.svg';
+import info from '../../assets/mainpage/info.svg';
+import recommend from '../../assets/mainpage/recommend.svg';
+import write from '../../assets/mainpage/write.svg';
 
 const Main = () => {
+    const isMentor = false;
+    const mentorTxt =
+        '재취업에 성공한 경력보유여성이신가요?\n잠깐 시간 내어 비슷한 어려움을 겪고 있는 후배들을 도와주세요!';
     return (
         <S.Wrapper>
             <Banner />
-            <S.InfoBox>
-                <S.TextBox>
-                    <S.Title fontSize='28px'>‘경력 인정’ 프로그램이란?</S.Title>
-                    <S.Text>
-                        경력보유여성의 경력 공백기에 대한 회고를 통해
-                        경력인정서도 발급받고 이력서와 자기소개서도 완성하기
-                        위한 전문 컨설턴트와의 1:1 프로그램
-                    </S.Text>
-                </S.TextBox>
-                <S.Button>프로그램 신청</S.Button>
-            </S.InfoBox>
-            <S.NewContainer>
+            {isMentor ? (
+                <S.InfoBox>
+                    <S.Title style={{ marginLeft: '2rem' }}>
+                        {mentorTxt}
+                    </S.Title>
+                    <S.Button>이력서 피드백하러 가기</S.Button>
+                </S.InfoBox>
+            ) : (
+                <></>
+            )}
+            <S.NewContainer margin='4rem 0 0 0'>
                 <S.InnerContainer>
-                    <S.Title>새로 등록된 합격 이력서를 확인해보세요!</S.Title>
-                    <S.InnerBox></S.InnerBox>
+                    <S.Title>어떤 일을 해야 할지 고민이신가요?</S.Title>
+                    <MenuBtn
+                        title='직무 백과'
+                        text='직무별 필요 역량과 근무 조건 알아보기'
+                        icon={dic}
+                    />
+                    <MenuBtn
+                        title='상황 기반 직무 추천'
+                        text='지금 내 상황에 맞는 추천 직무 알아보기'
+                        icon={recommend}
+                    />
                 </S.InnerContainer>
                 <S.InnerContainer>
-                    <S.Title>새로 등록된 질문에 대답해주세요!</S.Title>
-                    <S.InnerBox></S.InnerBox>
+                    <S.Title>하고 싶은 일이 있으신가요?</S.Title>
+                    <MenuBtn
+                        title='직업 교육'
+                        text='최신 교육과정을 통해 취업 역량 높이기'
+                        icon={edu}
+                    />
+                    {isMentor ? (
+                        <MenuBtn
+                            title='이력서 피드백'
+                            text='동종업계 후배들의 이력서 피드백하기'
+                            icon={write}
+                        />
+                    ) : (
+                        <MenuBtn
+                            title='이력서 작성'
+                            text='이력서 준비하고 동종업계 멘토 피드백 받기'
+                            icon={write}
+                        />
+                    )}
+                </S.InnerContainer>
+            </S.NewContainer>
+            <S.NewContainer margin='2rem 0 4rem 0'>
+                <S.InnerContainer>
+                    <S.Title>아이가 있으신가요?</S.Title>
+                    <MenuBtn
+                        title='인근 보육 시설 조회'
+                        text='믿고 맡길 수 있는 인근 보육 시설 알아보기'
+                        icon={child}
+                    />
+                </S.InnerContainer>
+                <S.InnerContainer>
+                    <S.Title>더 많은 도움이 필요하신가요?</S.Title>
+                    <MenuBtn
+                        title='취업 지원 사이트 모음'
+                        text='다양한 취업 지원 사이트 모아보기'
+                        icon={info}
+                    />
                 </S.InnerContainer>
             </S.NewContainer>
         </S.Wrapper>
