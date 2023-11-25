8cc8cbf3b349a773bdc68a9bad1e9f596f249be1
Hanby Choi <hanby.choi@ewhain.net>
2023-11-26 00:05:11 +0900

    feat: 직무 추천 api 1차 연결

--- src/components/recommendpage/Recommend.jsx
+++ src/components/recommendpage/Recommend.jsx
@@ -1,14 +1,38 @@
 import { S } from './Recommend.style';
 import PageTitle from '../_common/PageTitle';
 import RecommendResult from './RecommendResult';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { GetQuestion, PostAnswer } from '../../api/diagnosis';
 const Recommend = () => {
-    const [isTest, SetIsTest] = useState(true);
+    const [isTest, setIsTest] = useState(true);
+    const [question, setQuestion] = useState({});
+    const [answer, setAnswer] = useState({
+        q1: 0,
+        q2: 0,
+        q3: 0,
+        q4: 0,
+        q5: 0,
+        q6: 0,
+    });
+    const [result, setResult] = useState([]);
     const testTxt =
         '아직 어떤 일을 하면 좋을지 정하지 못하셨나요?\n어떤 일이 내 상황에 맞을지, 오래 할 수 있는 일일지 직무 추천 검사를 통해 알아보세요!';
-    const onResultClick = () => {
-        SetIsTest(false);
+
+    const GetQuestions = async () => {
+        const res = await GetQuestion();
+        console.log(res);
+        setQuestion(res.questions);
     };
+    const onResultClick = async answer => {
+        const res = await PostAnswer(answer);
+        console.log(res);
+        setResult(res.jobsResponse);
+        setIsTest(false);
+    };
+
+    useEffect(() => {
+        GetQuestions();
+    }, []);
 
     return (
         <S.Wrapper>
@@ -23,45 +47,34 @@ const Recommend = () => {
                     <S.InfoBox>
                         <S.InfoTitle>상황 기반 직무 추천 검사</S.InfoTitle>
                         <S.Line />
-                        <S.QuestionBox>
-                            <S.Question>1. 미성년인 자녀가 있나요?</S.Question>
-                            <div
-                                style={{
-                                    display: 'flex',
-                                    gap: '1rem',
-                                    marginRight: '2rem',
-                                }}
-                            >
-                                <S.CheckBox type='checkbox' />
-                                <S.Text marginL='0'>예</S.Text>
-                                <S.CheckBox type='checkbox' />
-                                <S.Text marginL='0'>아니오</S.Text>
-                            </div>
-                        </S.QuestionBox>
-                        <S.QuestionBox>
-                            <S.Question>
-                                2. 아이를 맡아줄 곳이 있나요?
-                            </S.Question>
-                            <div
-                                style={{
-                                    display: 'flex',
-                                    gap: '1rem',
-                                    marginRight: '2rem',
-                                }}
-                            >
-                                <S.CheckBox type='checkbox' />
-                                <S.Text marginL='0'>예</S.Text>
-                                <S.CheckBox type='checkbox' />
-                                <S.Text marginL='0'>아니오</S.Text>
-                            </div>
-                        </S.QuestionBox>
+                        {question.map(item => {
+                            <S.QuestionBox>
+                                <S.Question>{item.question}</S.Question>
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        gap: '1rem',
+                                        marginRight: '2rem',
+                                    }}
+                                >
+                                    <S.CheckBox type='checkbox' />
+                                    <S.Text marginL='0'>예</S.Text>
+                                    <S.CheckBox type='checkbox' />
+                                    <S.Text marginL='0'>아니오</S.Text>
+                                </div>
+                            </S.QuestionBox>;
+                        })}
                     </S.InfoBox>
                     <S.GreenBtn onClick={onResultClick}>
                         결과 확인하기
                     </S.GreenBtn>
                 </>
             ) : (
-                <RecommendResult isTest={isTest} setIsTest={SetIsTest} />
+                <RecommendResult
+                    isTest={isTest}
+                    setIsTest={setIsTest}
+                    result={result}
+                />
             )}
         </S.Wrapper>
     );
--- src/components/recommendpage/RecommendResult.jsx
+++ src/components/recommendpage/RecommendResult.jsx
@@ -1,6 +1,15 @@
 import { S } from './Recommend.style';
 import PageTitle from '../_common/PageTitle';
-const RecommendResult = ({ isTest, setIsTest }) => {
+import { useNavigate } from 'react-router';
+
+const RecommendResult = ({ isTest, setIsTest, result }) => {
+    const navigate = useNavigate();
+    let jobList = '';
+    for (let i in result) {
+        jobList += i.name + ', ';
+    }
+    jobList = jobList.slice(0, -2);
+
     return (
         <>
             <PageTitle text={'상황 기반 직무 추천 결과'} />
@@ -12,50 +21,46 @@ const RecommendResult = ({ isTest, setIsTest }) => {
                 }}
             >
                 <S.Text>나에게 가장 잘 맞는 직무 리스트</S.Text>
-                <S.ResultTitle>
-                    독서 논술 지도사, 요가 지도사, 체험학습 강사, 학원 지도사
-                </S.ResultTitle>
+                <S.ResultTitle>{jobList}</S.ResultTitle>
             </S.GrayBox>
             <S.InfoBox style={{ marginBottom: '4rem' }}>
                 <S.InlineContainer>
                     <S.InfoTitle>추천 직무 더 알아보기</S.InfoTitle>
                     <div style={{ fontSize: '0.8rem', fontWeight: '600' }}>
-                        총 2건
+                        총 {result.length}건
                     </div>
                 </S.InlineContainer>
                 <S.Line />
-                <S.ResultContainer>
-                    <S.RowContainer>
-                        <S.Thumbnail />
-                        <S.Text>보육교사</S.Text>
-                    </S.RowContainer>
-                    <div
-                        style={{
-                            display: 'flex',
-                            gap: '1rem',
-                            marginRight: '2rem',
-                        }}
-                    >
-                        <S.Button>추천 직무 백과 바로가기</S.Button>
-                        <S.Button>추천 직업 교육 바로가기</S.Button>
-                    </div>
-                </S.ResultContainer>
-                <S.ResultContainer>
-                    <S.RowContainer>
-                        <S.Thumbnail />
-                        <S.Text>보육교사</S.Text>
-                    </S.RowContainer>
-                    <div
-                        style={{
-                            display: 'flex',
-                            gap: '1rem',
-                            marginRight: '2rem',
-                        }}
-                    >
-                        <S.Button>추천 직무 백과 바로가기</S.Button>
-                        <S.Button>추천 직업 교육 바로가기</S.Button>
-                    </div>
-                </S.ResultContainer>
+                {result.map(item => {
+                    <S.ResultContainer key={item.jobId}>
+                        <S.RowContainer>
+                            <S.Thumbnail src={item.imageUrl} />
+                            <S.Text>{item.name}</S.Text>
+                        </S.RowContainer>
+                        <div
+                            style={{
+                                display: 'flex',
+                                gap: '1rem',
+                                marginRight: '2rem',
+                            }}
+                        >
+                            <S.Button
+                                onClick={() =>
+                                    navigate(`/dictionary/${item.jobId}`)
+                                }
+                            >
+                                추천 직무 백과 바로가기
+                            </S.Button>
+                            <S.Button
+                                onClick={() =>
+                                    navigate(`/training/${item.jobId}`)
+                                }
+                            >
+                                추천 직업 교육 바로가기
+                            </S.Button>
+                        </div>
+                    </S.ResultContainer>;
+                })}
                 <div>
                     <S.RowContainer
                         style={{ justifyContent: 'center', gap: '1.5rem' }}
@@ -63,7 +68,9 @@ const RecommendResult = ({ isTest, setIsTest }) => {
                         <S.GreenBtn onClick={() => setIsTest(true)}>
                             검사 다시 하기
                         </S.GreenBtn>
-                        <S.GreenBtn>이력서 작성 바로가기</S.GreenBtn>
+                        <S.GreenBtn onClick={() => navigate('/resume')}>
+                            이력서 작성 바로가기
+                        </S.GreenBtn>
                     </S.RowContainer>
                 </div>
             </S.InfoBox>
