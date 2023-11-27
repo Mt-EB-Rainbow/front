a9de4a546d11fedec888c989f62ccbc6ccfef619
aoqlsdl <hyeeev@gmail.com>
2023-11-27 14:37:20 +0900

    feat: 이력서 불러오기

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -38,38 +38,43 @@ const Resume = () => {
                     <PageTitle text={'내 이력서 관리'} />
                     <div style={{ height: '6.5rem' }}></div>
                     <BoardsHeader
-                        length={0}
+                        length={resumeResponses.length}
                         placeholder={'제목 및 내용으로 검색'}
                     />
-
+                    {/* id 역순으로 반환 */}
                     {resumeResponses ? (
                         <>
-                            {resumeResponses.map(el => (
-                                <S.Boards key={el.resumeId}>
-                                    <S.Info>
-                                        <S.Name
-                                            onClick={() => {
-                                                navigate(
-                                                    `/detail/${el.resumeId}`,
-                                                );
-                                            }}
-                                        >
-                                            {el.title}
-                                        </S.Name>
-                                        <S.JobInfo>2023.10.01</S.JobInfo>
-
-                                        <S.GreenBox>사무보조</S.GreenBox>
-                                    </S.Info>
-                                    <S.Right>
-                                        {/* {isopend? "공개" : "비공개"} 공개면 dark-gray, 비공개면 gray*/}
-                                        <S.Open>공개</S.Open>
-                                        {/* 수정 / 삭제 버튼 */}
-
-                                        <S.Rewrite>수정</S.Rewrite>
-                                        <S.Delete>삭제</S.Delete>
-                                    </S.Right>
-                                </S.Boards>
-                            ))}
+                            {[...resumeResponses]
+                                .sort((a, b) => b.resumeId - a.resumeId)
+                                .map(el => (
+                                    <S.Boards key={el.resumeId}>
+                                        <S.Info>
+                                            <S.Name
+                                                onClick={() => {
+                                                    navigate(
+                                                        `/detail/${el.resumeId}`,
+                                                    );
+                                                }}
+                                            >
+                                                {el.title}
+                                            </S.Name>
+                                            <S.JobInfo>
+                                                {el.modifiedAt}
+                                            </S.JobInfo>
+                                            <S.GreenBox>
+                                                사무보조
+                                                {/* 이거 api로 안 넘어와서 수정해야 할듯? */}
+                                            </S.GreenBox>
+                                        </S.Info>
+                                        <S.Right>
+                                            {/* {isopened? "공개" : "비공개"} 공개면 dark-gray, 비공개면 gray*/}
+                                            <S.Open>공개</S.Open>
+                                            {/* 수정 / 삭제 버튼 */}
+                                            <S.Rewrite>수정</S.Rewrite>
+                                            <S.Delete>삭제</S.Delete>
+                                        </S.Right>
+                                    </S.Boards>
+                                ))}
                             <div style={{ marginTop: '5rem' }}>
                                 <GreenBtn
                                     text={'새 이력서 추가'}
