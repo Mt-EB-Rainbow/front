171481102afafc2f667c3bce33ade25419643efb
aoqlsdl <hyeeev@gmail.com>
2023-11-27 19:37:50 +0900

    fix: 멘토 여부 확인 버그 수정

--- src/api/isMentor.js
+++ src/api/isMentor.js
@@ -4,8 +4,8 @@ export const CheckMentorApi = async memberId => {
     try {
         const res = await client.get(`/auth/check/${memberId}`);
 
-        console.log(res, '멘토 확인 완료');
-        return res;
+        console.log(res.data, '멘토 확인 완료');
+        return res.data;
     } catch (err) {
         console.log(err);
     }
